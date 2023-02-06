1452a8827a22501d6f109fd99cda2d50c7f1cf36
LeeorBentalIsraeli <leeor2003@gmail.com>
2023-02-06 18:42:12 +0200

    Assignment 2 edits + assignment 1

--- Basic/Solution.cs
+++ Basic/Solution.cs
@@ -10,9 +10,7 @@ namespace Basic
     {
         public static void Assignment1()
         {
-            /*
-             * Write code here
-             */
+            Console.WriteLine("Hello panda!");
         }
 
         public static void Assignment2()
@@ -22,6 +20,7 @@ namespace Basic
             double gradeCount = 0;
             int maxGrade = 0;
             int minGrade = 0;
+            Console.WriteLine("Enter Grades: ");
             int input = Convert.ToInt32(Console.ReadLine());
             while (input != -1)
             {
@@ -39,13 +38,14 @@ namespace Basic
                     numberFails = numberFails + input;
                 }
                 gradeCount = gradeCount + 1;
+                input = Convert.ToInt32(Console.ReadLine());
             }
             double gradeAverage = -1;
             if (gradeCount != 0)
             {
                 gradeAverage = gradeSum / gradeCount;
+                Console.WriteLine("Number of fails: " + numberFails + " Average: " + gradeAverage + " Minimum grade: " + minGrade + " Maximum grade: " + maxGrade);
             }
-            Console.WriteLine("Number of fails: " + numberFails + " Average: " + gradeAverage + " Minimum grade: " + minGrade + " Maximum grade: " + maxGrade);
             else
             {
                 Console.WriteLine("No grades entered");
