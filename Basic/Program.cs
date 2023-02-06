302103288d521d211acffcc2ffff148715541780
NatalieShaked <shakednatalie@gmail.com>
2023-02-06 18:21:36 +0200

    commented out program.

--- Basic/Program.cs
+++ Basic/Program.cs
@@ -2,7 +2,7 @@
 using Basic;
 
 Console.WriteLine("Checking your solutions...");
-//Helper.ManageAssignment(1, Solution.Assignment1, "Hello panda!");
+Helper.ManageAssignment(1, Solution.Assignment1, "Hello panda!");
 //Helper.ManageAssignment(2, Solution.Assignment2, "Hello panda!");
-Helper.ManageAssignment(3, Solution.Assignment3, "Hello panda!");
-Helper.ManageAssignment(4, Solution.Assignment4, "Hello panda!");
+//Helper.ManageAssignment(3, Solution.Assignment3, "Hello panda!");
+//Helper.ManageAssignment(4, Solution.Assignment4, "Hello panda!");
