17e3cc42414ba8780a8a199dad7a8cfd140d4b81
Assaf Hillel <68805670+amazingProj@users.noreply.github.com>
2023-02-06 17:28:33 +0200

    Assignment 1

--- Basic/Program.cs
+++ Basic/Program.cs
@@ -2,4 +2,5 @@
 using Basic;
 
 Console.WriteLine("Checking your solutions...");
-Helper.ManageAssignment(1, Solution.Assignment1, "Hello panda!");
+Helper.ManageAssignment(1, Solution.Assignment2, "Hello panda!");
+
--- Basic/Solution.cs
+++ Basic/Solution.cs
@@ -10,16 +10,38 @@ namespace Basic
     {
         public static void Assignment1()
         {
-            /*
-             * Write code here
-             */
+            Console.WriteLine("Hello panda!");
         }
 
         public static void Assignment2()
         {
-            /*
-             * Write code here
-             */
+            string input;
+            int number = 0;
+            int sum = 0;
+            int numbers = 0;
+            int max = int.MinValue;
+            int min = int.MaxValue;
+            Console.WriteLine("Enter numbers. Press -1 to exit");
+            while (number != -1)
+            {
+                input = Console.ReadLine();
+                if (input is not null)
+                {
+                    number = Convert.ToInt32(input);
+                    sum += number;
+                    ++numbers;
+                    if (number > max)
+                    {
+                        max = number;
+                    }
+                    if (number < min)
+                    {
+                        min = number;
+                    }
+                }
+            }
+            double avg = sum / numbers;
+            Console.WriteLine($"The average is {avg}, \nThe maximum is {max}\nThe minimum is {min}");
         }
 
         public static void Assignment3()
