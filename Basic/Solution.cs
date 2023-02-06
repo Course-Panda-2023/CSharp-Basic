629835c3c9538bcecc4b3626975ee4d02cb5a2bc
Ozbahji <ozbakhajian@gmail.com>
2023-02-06 18:12:27 +0200

    Assignment 3

--- Basic/Solution.cs
+++ Basic/Solution.cs
@@ -52,9 +52,22 @@ namespace Basic
 
         public static void Assignment3()
         {
-            /*
-             * Write code here
-             */
+            for (int i=1; i<=1000; i++) {
+                if (i % 5 == 0 && i % 3 == 0) 
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if ( i % 5 ==0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if ( i % 3 == 0 )
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else
+                    Console.WriteLine(i); 
+            }
         }
 
         public static void Assignment4()
