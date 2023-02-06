9cd5a9c2d0d9f0dfcebf77848518d9e3d5de7c2b
omri2084 <omri2084@cs.huji.ac.il>
2023-02-06 17:22:57 +0200

    finish Assignment1 - Assignment4

--- Basic/Solution.cs
+++ Basic/Solution.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System;
+
+
+
+
+
+
+
 
 namespace Basic
 {
@@ -10,30 +18,77 @@ namespace Basic
     {
         public static void Assignment1()
         {
-            /*
-             * Write code here
-             */
+            Console.WriteLine("!panda Hello!");
         }
 
         public static void Assignment2()
         {
-            /*
-             * Write code here
-             */
+            int total = 0;
+            int high = int.MinValue;
+            int low = int.MaxValue;
+            int count = 0;
+            Console.WriteLine("enter number:");
+            string num = Console.ReadLine();
+            int intnum = Convert.ToInt32(num);
+            while (intnum != -1)
+            {
+                if (intnum > high)
+                {
+                    high = intnum;
+                }
+                else if (intnum < low) 
+                {
+                    low = intnum; 
+                }
+                count++;
+                total += intnum;
+                num = Console.ReadLine();
+                intnum = Convert.ToInt32(num);
+            }
+            Console.WriteLine((double)total / count);
         }
 
         public static void Assignment3()
         {
-            /*
-             * Write code here
-             */
+
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0) 
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (i % 3 == 0) 
+                { 
+                    Console.WriteLine("Fizz");
+                }
+                else if (i % 5 == 0)
+                { 
+                    Console.WriteLine("Buzz");
+                }
+                else
+                { 
+                    Console.WriteLine(i); 
+                }
+            }
         }
 
         public static void Assignment4()
         {
-            /*
-             * Write code here
-             */
+            string num = Console.ReadLine();
+            int baseNum = Convert.ToInt32(num);
+            num = Console.ReadLine();
+            int intnum = Convert.ToInt32(num);
+            bool flag = true;
+            while (intnum != -1)
+            {
+                if (intnum % baseNum != 0)
+                {
+                    flag = false;
+                }
+                num = Console.ReadLine();
+                intnum = Convert.ToInt32(num);
+            }
+            Console.WriteLine(flag);
         }
     }
 }
