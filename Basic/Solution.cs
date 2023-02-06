f077ec927959f652293727791388d850699b1d40
noayush <noayush@gmail.com>
2023-02-06 17:56:51 +0200

    assignment 1

--- Basic/Solution.cs
+++ Basic/Solution.cs
@@ -13,6 +13,7 @@ namespace Basic
             /*
              * Write code here
              */
+            Console.WriteLine("Hello panda!");
         }
 
         public static void Assignment2()
@@ -20,6 +21,32 @@ namespace Basic
             /*
              * Write code here
              */
+            // Console.WriteLine("Enter grades:");
+            double grade = Convert.ToDouble(Console.ReadLine()!);
+            double avgGrade = 0;
+            double maxGrade = 0;
+            double minGrade = 101;
+            int failNum = 0;
+            int numGrades = 0;
+            while (grade != -1) { 
+                avgGrade += grade;
+                if (grade > maxGrade)
+                {
+                    maxGrade = grade;
+                }
+                if (grade < minGrade)
+                {
+                    minGrade = grade;
+                }
+                if (grade < 60)
+                {
+                    failNum++;
+                }
+                numGrades++;
+                grade = Convert.ToDouble(Console.ReadLine()!);
+            }
+            avgGrade /= numGrades;
+            Console.WriteLine($"The average grade is: {avgGrade}\nThe maximal grade is: {maxGrade}\nThe minimal grade is: {minGrade}\nThe number of fails is: {failNum}");
         }
 
         public static void Assignment3()
