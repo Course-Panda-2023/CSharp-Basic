63957779574ec068cf9c428e08b6e767e2ed75fd
EYTZIP <eyaltzipor5@gmail.com>
2023-02-06 17:37:12 +0200

    finished all the asignments

--- Basic/Solution.cs
+++ Basic/Solution.cs
@@ -10,30 +10,65 @@ namespace Basic
     {
         public static void Assignment1()
         {
-            /*
-             * Write code here
-             */
+            Console.WriteLine("Hello panda!");
         }
 
         public static void Assignment2()
         {
-            /*
-             * Write code here
-             */
+            int grade;
+            int min = 100;
+            int avr = 0;
+            int max = 0;
+            int num = 0;
+            int numOfFailed = 0;
+            Console.WriteLine("Enter your grade: ");
+            grade = int.Parse(Console.ReadLine());
+            while (grade >= 0)
+            {
+                avr += grade;
+                num++;
+                if (grade < 60)
+                    numOfFailed++;
+                if (max < grade)
+                    max = grade;
+                else if (min > grade)
+                    min = grade;
+                Console.WriteLine("Enter another grade: ");
+                grade = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("The average is: " + Math.Round((double)avr / (double)num, 2) + "\nMax: " + max + "\nMin: " + min + "\nFailed: " + numOfFailed);
+
         }
 
         public static void Assignment3()
         {
-            /*
-             * Write code here
-             */
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i % 15 == 0)
+                    Console.WriteLine("FizzBuzz");
+                else if (i % 5 == 0)
+                    Console.WriteLine("Buzz");
+                else if (i % 3 == 0)
+                    Console.WriteLine("Fizz");
+                else Console.WriteLine(i);
+            }
         }
 
         public static void Assignment4()
         {
-            /*
-             * Write code here
-             */
+            int num = 0;
+            Boolean divide3 = true;
+            Console.WriteLine("Enter number");
+            num = int.Parse(Console.ReadLine());
+            while (num >= 0)
+            {
+                if (divide3 && num % 3 != 0)
+                    divide3 = false;
+                Console.WriteLine("Enter another number: ");
+                num = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(divide3);
+
         }
     }
 }
