df52c039c8b6a8c46f9bffdceb4f31947e38a8cb
ahmichal <95378632+ahmichal@users.noreply.github.com>
2023-02-06 19:33:08 +0200

    adding second assinment

--- Basic/Solution.cs
+++ Basic/Solution.cs
@@ -15,9 +15,48 @@ namespace Basic
 
         public static void Assignment2()
         {
-            /*
-             * Write code here
-             */
+            int low = 100;
+            int high = 0;
+            int fail = 0;
+            double avg = 0;
+            int numofgrades = 0;
+            int grade = 0;
+            while (grade != -1)
+            {
+                Console.WriteLine("enter grade, enter -1 to stop: ");
+                string gradeStr = Console.ReadLine();
+                grade = Convert.ToInt32(gradeStr);
+                if (grade < 60)
+                {
+                    fail += 1;
+                }
+                if (grade >= 0 && grade <= 100)
+                {
+                    numofgrades += 1;
+                    avg += grade;
+                    if (low > grade)
+                    {
+                        low = grade;
+                    }
+                    if (high < grade)
+                    {
+                        high = grade;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("invalid grade ");
+                }
+
+            }
+            if (numofgrades != 0)
+            {
+                avg = avg / numofgrades;
+            }
+            Console.WriteLine("avg: " + avg);
+            Console.WriteLine("low: " + low);
+            Console.WriteLine("high: " + high);
+            Console.WriteLine("fail: " + fail);
         }
 
         public static void Assignment3()
