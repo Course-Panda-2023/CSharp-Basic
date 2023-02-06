32a7314ee996c73f3a46ce5dc7735f1825b2ec68
noayush <noayush@gmail.com>
2023-02-06 18:27:24 +0200

    Assignment 2

--- Basic/Solution.cs
+++ Basic/Solution.cs
@@ -54,6 +54,22 @@ namespace Basic
             /*
              * Write code here
              */
+            for (int i=1; i<=1000; i++)
+            {
+                if (i%3 == 0)
+                {
+                    Console.Write("Fizz");
+                }
+                if (i%5 == 0) 
+                {
+                    Console.Write("Buzz");
+                } 
+                if (i%3 != 0 && i%5 != 0)
+                {
+                    Console.Write($"{i}");
+                }
+                Console.Write("\n");
+            }
         }
 
         public static void Assignment4()
