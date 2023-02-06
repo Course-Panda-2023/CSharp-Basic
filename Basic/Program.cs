1b1dc2203c701fe2387c77afe379184d0ac29f4c
razbroc <razbroc@gmail.com>
2023-02-06 18:07:45 +0200

    add(assignments one+two)

--- Basic/Program.cs
+++ Basic/Program.cs
@@ -1,5 +1,1 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Basic;
-
-Console.WriteLine("Checking your solutions...");
-Helper.ManageAssignment(1, Solution.Assignment1, "Hello panda!");
+﻿using Basic;
--- Basic/Solution.cs
+++ Basic/Solution.cs
@@ -10,23 +10,52 @@ namespace Basic
     {
         public static void Assignment1()
         {
-            /*
-             * Write code here
-             */
+            Console.WriteLine("Hello panda!");
         }
 
         public static void Assignment2()
         {
-            /*
-             * Write code here
-             */
+            const int STOP_INPUT = -1;
+            const int PASS_GRADE = 60;
+
+            double gradeSum = 0;
+            int highestGrade = 0;
+            int lowetGrade = 0;
+            int numOfFails = 0;
+            int gradeCounter = 0;
+
+            Console.WriteLine("please enter a grade: ");
+            int currGrade = int.Parse(Console.ReadLine());
+
+            while (currGrade != STOP_INPUT)
+            {
+                gradeCounter++;
+                gradeSum += currGrade;
+                highestGrade = highestGrade < currGrade ? currGrade : highestGrade;
+                lowetGrade = lowetGrade > currGrade ? currGrade : lowetGrade;
+                numOfFails = currGrade < PASS_GRADE ? numOfFails + 1 : numOfFails;
+            }
+            Console.WriteLine($"Grade avarage is: {Math.Round(gradeSum / gradeCounter, 2)} , the highest grade is: {highestGrade}, " +
+                $"and the lowest grade is: {lowetGrade}. the number of fails is: {numOfFails}");
+            
         }
 
         public static void Assignment3()
         {
-            /*
-             * Write code here
-             */
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i%3==0)
+                {
+                    if (i % 5 == 0)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                    }
+                    Console.WriteLine("FizzBuzz");
+                } else 
+                { 
+                    Console.WriteLine(i);
+                }
+            }
         }
 
         public static void Assignment4()
